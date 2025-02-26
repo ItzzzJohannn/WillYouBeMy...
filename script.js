11b80ce9ff1828ded649b47579f5763165d557b6
ItzzzJohannn <sonarbangladesh524@gmail.com>
2025-02-26 16:41:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const yesButton = document.getElementById("yes");
 const noButton = document.getElementById("no");
-const message = document.getElementById("message");
 
 const noPrompts = [
-  "Are you sure? :(",
-  "Really sure?",
-  "Do you really mean no?",
-  "Don't you feel it?"
+    "Are you sure? :(",
+    "Really sure? ;-;",
+    "Don't break my heart! ;-;",
+    "Think again... >_<",
+    "You might regret this... :(",
+    "But I like you... :'>",
+    "Last chance!",
+    "You can't escape love ❤️"
 ];
 
 let noClickCount = 0;
 
 noButton.addEventListener("click", function () {
-  if (noClickCount < noPrompts.length) {
-    noButton.innerText = noPrompts[noClickCount];
-  } else {
-    noButton.innerText = "Can't say no to love!";
-  }
-  noClickCount++;
-  // Make the Yes button bigger each time No is clicked
-  yesButton.style.transform = `scale(${1 + noClickCount * 0.2})`;
+    // Cycle through the prompts infinitely
+    noButton.innerText = noPrompts[noClickCount % noPrompts.length];
+    
+    // Increase the size of the Yes button
+    yesButton.style.transform = `scale(${1 + noClickCount * 0.2})`;
+    
+    noClickCount++;
 });
 
 yesButton.addEventListener("click", function () {
-  message.innerText = "Yay! I’m so happy! (✿◕‿◕)";
+    noButton.innerText = "Too late now! ❤️";
+    yesButton.innerText = "Yay! (✿◕‿◕)";
 });
